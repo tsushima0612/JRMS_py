ea8e33d61a3049317e14dce1be4b8e75349b4601
津嶋康平 <k.tsushima.sys22@morijyobi.ac.jp>
2023-06-13 15:05:32 +0900

    フォームの作成

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app= Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/register_accounts')
+def register_accounts():
+    return render_template('register_accounts.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
